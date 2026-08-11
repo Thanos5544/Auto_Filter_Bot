74e4f50784689c4d0b3ddcdc3615c44dedf8fdad
Thanos5544 <thanos554433@gmail.com>
2026-08-11 15:15:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,7 +15,7 @@ from database.config_db import mdb
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup
 from pyrogram import Client, filters, enums, StopPropagation
 from pyrogram.errors import FloodWait, UserNotParticipant , ChannelInvalid, PeerIdInvalid
-from database.ia_filterdb import Media, Media2, Media3, get_file_details, unpack_new_file_id, get_bad_files, save_file
+from database.ia_filterdb import Media, Media2, Media3, Media4, get_file_details, unpack_new_file_id, get_bad_files, save_file
 from database.users_chats_db import db
 from info import (
     LOG_CHANNEL, IMDB_TEMPLATE, IS_VERIFY, TUTORIAL, TUTORIAL_2, TUTORIAL_3, EMOJI_MODE, REACTIONS,
