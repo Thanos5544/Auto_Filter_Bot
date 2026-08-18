fa4d58cc97fae89e203dffedc12779eb4fa4b531
Thanos5544 <thanos554433@gmail.com>
2026-08-18 16:23:50 +0530

    Update render_template.py

--- dreamxbotz/util/render_template.py
+++ dreamxbotz/util/render_template.py
@@ -1,42 +1,30 @@
-#Thanks @dreamxbotz for helping in this journey 
+import aiohttp
 import jinja2
-from info import BIN_CHANNEL, URL
-from dreamxbotz.Bot import dreamxbotz
-from dreamxbotz.util.human_readable import humanbytes
-from dreamxbotz.util.file_properties import get_file_ids
-from dreamxbotz.server.exceptions import InvalidHash
 import urllib.parse
-import logging
+from FileStream.config import Telegram, Server
+from FileStream.utils.database import Database
+from FileStream.utils.human_readable import humanbytes
+db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
 
-async def render_page(id, secure_hash, src=None):
-    file = await dreamxbotz.get_messages(int(BIN_CHANNEL), int(id))
-    file_data = await get_file_ids(dreamxbotz, int(BIN_CHANNEL), int(id))
-    if file_data.unique_id[:6] != secure_hash:
-        logging.debug(f"link hash: {secure_hash} - {file_data.unique_id[:6]}")
-        raise InvalidHash
-    file_name_quoted = urllib.parse.quote(file_data.file_name)
-    src = urllib.parse.urljoin(URL, f"{id}/{file_name_quoted}?hash={secure_hash}")
-    download_src = urllib.parse.urljoin(URL, f"dl/{id}/{file_name_quoted}?hash={secure_hash}")
-    tag = file_data.mime_type.split("/")[0].strip() if file_data.mime_type else ""
-    file_size = humanbytes(file_data.file_size)
-    if tag in ["video", "audio"]:
-        template_file = "dreamxbotz/template/req.html"
+async def render_page(db_id):
+    file_data=await db.get_file(db_id)
+    src = urllib.parse.urljoin(Server.URL, f'dl/{file_data["_id"]}')
+    file_size = humanbytes(file_data['file_size'])
+    file_name = file_data['file_name'].replace("_", " ")
+
+    if str((file_data['mime_type']).split('/')[0].strip()) == 'video':
+        template_file = "FileStream/template/play.html"
     else:
-        template_file = "dreamxbotz/template/dl.html"
+        template_file = "FileStream/template/dl.html"
+        async with aiohttp.ClientSession() as s:
+            async with s.get(src) as u:
+                file_size = humanbytes(int(u.headers.get('Content-Length')))
+
     with open(template_file) as f:
         template = jinja2.Template(f.read())
-    file_name = file_data.file_name.replace("_", " ")
-    vlc_url = f"vlc://{src}"
-    mx_url = f"intent:{src}#Intent;package=com.mxtech.videoplayer.ad;S.title={file_name_quoted};end"
-    nplayer_url = f"intent:{src}#Intent;package=com.genuine.leone;S.title={file_name_quoted};end"
+
     return template.render(
         file_name=file_name,
         file_url=src,
-        download_url=download_src,
-        file_size=file_size,
-        file_unique_id=file_data.unique_id,
-        mime_type=file_data.mime_type,
-        vlc_url=vlc_url,
-        mx_url=mx_url,
-        nplayer_url=nplayer_url,
+        file_size=file_size
     )
