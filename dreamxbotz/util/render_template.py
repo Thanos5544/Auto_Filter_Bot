8b8927acad1e5157b198b1135bbac25dd509f863
Thanos5544 <thanos554433@gmail.com>
2026-08-18 17:23:13 +0530

    Update render_template.py

--- dreamxbotz/util/render_template.py
+++ dreamxbotz/util/render_template.py
@@ -1,47 +1,54 @@
-#Thanks @dreamxbotz for helping in this journey 
-
 import jinja2
-from info import *
+import urllib.parse
+import logging
+
+from info import BIN_CHANNEL, URL
 from dreamxbotz.Bot import dreamxbotz
 from dreamxbotz.util.human_readable import humanbytes
 from dreamxbotz.util.file_properties import get_file_ids
 from dreamxbotz.server.exceptions import InvalidHash
-import urllib.parse
-import logging
-import aiohttp
 
 
 async def render_page(id, secure_hash, src=None):
-    file = await dreamxbotz.get_messages(int(BIN_CHANNEL), int(id))
-    file_data = await get_file_ids(dreamxbotz, int(BIN_CHANNEL), int(id))
-    if file_data.unique_id[:6] != secure_hash:
-        logging.debug(f"link hash: {secure_hash} - {file_data.unique_id[:6]}")
-        logging.debug(f"Invalid hash for message with - ID {id}")
-        raise InvalidHash
-
-    src = urllib.parse.urljoin(
-        URL,
-        f"{id}/{urllib.parse.quote_plus(file_data.file_name)}?hash={secure_hash}",
+    file_data = await get_file_ids(
+        dreamxbotz,
+        int(BIN_CHANNEL),
+        int(id)
     )
 
-    tag = file_data.mime_type.split("/")[0].strip()
+    if file_data.unique_id[:6] != secure_hash:
+        logging.debug(
+            f"Invalid hash for message ID {id}: "
+            f"{secure_hash} != {file_data.unique_id[:6]}"
+        )
+        raise InvalidHash
+
+    file_name = file_data.file_name or f"file_{id}"
+
+    # FileStream-style: player ka source direct /dl endpoint hoga
+    encoded_name = urllib.parse.quote(file_name, safe="")
+    file_url = urllib.parse.urljoin(
+        URL,
+        f"dl/{id}/{encoded_name}?hash={secure_hash}"
+    )
+
+    mime_type = file_data.mime_type or "application/octet-stream"
     file_size = humanbytes(file_data.file_size)
-    if tag in ["video", "audio"]:
+
+    media_type = mime_type.split("/", 1)[0].lower()
+
+    if media_type in ("video", "audio"):
         template_file = "dreamxbotz/template/req.html"
     else:
         template_file = "dreamxbotz/template/dl.html"
-        async with aiohttp.ClientSession() as s:
-            async with s.get(src) as u:
-                file_size = humanbytes(int(u.headers.get("Content-Length")))
 
-    with open(template_file) as f:
+    with open(template_file, "r", encoding="utf-8") as f:
         template = jinja2.Template(f.read())
 
-    file_name = file_data.file_name.replace("_", " ")
-
     return template.render(
-        file_name=file_name,
-        file_url=src,
+        file_name=file_name.replace("_", " "),
+        file_url=file_url,
         file_size=file_size,
         file_unique_id=file_data.unique_id,
+        mime_type=mime_type,
     )
